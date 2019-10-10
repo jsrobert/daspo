b2ea0276412aa9e7a681dfa34ebe8475c0674f21
Jeff Roberts <jstephenroberts@hotmail.com>
2019-10-10 19:52:11 -0400

    gitignore

--- Clean-NpmNodeYarn.ps1
+++ Clean-NpmNodeYarn.ps1
@@ -1,8 +1,12 @@
 ﻿npm cache clean --force
 
 #cd $env:APPDATA\npm-cache
-cd $env:HOMEPATH
+Set-Location $env:HOMEPATH
 Get-ChildItem -Directory -Recurse -Include node_modules | Sort-Object -Property FullName | Select-Object -Property FullName
+
+$scriptsDir = Get-ChildItem -Directory -Recurse -Include scripts
+write-Output "Scripts directory = $scriptsDir"
+
 Get-ChildItem -Directory -Recurse -Include event-stream | Remove-Item
 Get-ChildItem -Directory -Recurse -Include flatmap-stream | Remove-Item
 
@@ -14,13 +18,13 @@ foreach($sf in $sourceFolders) {
     $nm.FullName
 }
 
-$dirs = Get-ChildItem -Directory | % { $_.GetDirectories("node_modules") } | % { $_.FullName }
-$dirs | % { Remove-Item $_ -Recurse -Force }
-Get-ChildItem -Directory -Filter node_modules -Recurse | % { Remove-Item $_.FullName -Recurse -Force }
+$dirs = Get-ChildItem -Directory | ForEach-Object { $_.GetDirectories("node_modules") } | ForEach-Object { $_.FullName }
+$dirs | ForEach-Object { Remove-Item $_ -Recurse -Force }
+Get-ChildItem -Directory -Filter node_modules -Recurse | ForEach-Object { Remove-Item $_.FullName -Recurse -Force }
 Get-ChildItem -Directory -Filter node_modules -Recurse 
 
-Get-ChildItem -Directory | % { Get-ChildItem $_.Name -Directory -Recurse -Include event-stream } | % { $_.FullName }
-Get-ChildItem -Directory | % { Get-ChildItem $_.Name -Directory -Recurse -Include node_modules } | % { Remove-Item $_.FullName -Recurse -Force }
+Get-ChildItem -Directory | ForEach-Object { Get-ChildItem $_.Name -Directory -Recurse -Include event-stream } | ForEach-Object { $_.FullName }
+Get-ChildItem -Directory | ForEach-Object { Get-ChildItem $_.Name -Directory -Recurse -Include node_modules } | ForEach-Object { Remove-Item $_.FullName -Recurse -Force }
 
 $topLevelNodeModules = Get-ChildItem -Filter node_modules -ErrorAction SilentlyContinue
 
